2b0a9fb988b169775c8bb0c358d9572911658014
KenOokamiHoro <horo@yoitsu.moe>
2017-05-06 23:01:31 +0800

    add devi

--- modules/commands/wiki.py
+++ modules/commands/wiki.py
@@ -14,6 +14,16 @@ def search_wikipedia(keyword,lang="en"):
     return (query[2][0],query[3][0])
     
 
+def search_mediawiki(api,keyword,mode="opensearch",textextract=False):
+    baseurl = '{}?action={}&search={}&redirects=resolve&format=json&utf8=true'
+    query = requests.get(baseurl.format(api,mode,keyword)).json()
+    if not query[1]:
+        raise NoResultException()
+    if textextract:
+        return (query[2][0],query[3][0])
+    else:
+        return (query[3][0],)
+    
 @asyncio.coroutine
 def wiki(arg,send):
     lang = arg.get("lang","en")
@@ -27,11 +37,24 @@ def wiki(arg,send):
     else:
         send("{} -> {} ".format(*result))
 
+@asyncio.coroutine
+def devi(arg,send):
+    keyword = arg['keyword']
+    try:
+        result = search_mediawiki("https://wikidevi.com/w/api.php",keyword)
+    except NoResultException:
+        send("╮(￣▽￣)╭ sad story... No results found.")
+    else:
+        send("{}".format(*result))
 
 help = [
     ('wiki'          , 'wiki[:lang] <keyword> -- Get <keyword> in <lang> wikipedia.'),
+    ('devi'          , 'devi <keyword> -- Get <keyword> in wikidevi.org'),
+    
 ]
 
 func = [
     (wiki            , r"wiki(?::(?P<lang>\S+))?\s+(?P<keyword>.+)"),
+    (devi            , r"devi\s+(?P<keyword>.+)"),
+    
 ]
